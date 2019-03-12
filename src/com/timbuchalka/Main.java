204d7002c3e251357161ec1f91306dbc31eeb679
locutus1986 <locutus1986@localhost.localdomain>
2019-03-12 18:44:14 -0400

    first

---
+++ src/com/timbuchalka/Main.java
@@ -0,0 +1,18 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public static boolean isEvenNumber(int x){
+        boolean result = false;
+
+        if(x%2 == 0){
+            result = true;
+        }
+
+        return result;
+    }
+}

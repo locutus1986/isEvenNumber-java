ff51249f51fad3315968229004163cae1b38e64c
locutus1986 <locutus1986@localhost.localdomain>
2019-03-12 18:49:59 -0400

    added while loop

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -3,7 +3,16 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int x = 4;
+	    int finishNumber = 20;
+
+	    while(x <= finishNumber){
+	        x++;
+	        if(!isEvenNumber(x)){
+	            continue;
+            }
+            System.out.println("Even number " + x);
+        }
     }
 
     public static boolean isEvenNumber(int x){

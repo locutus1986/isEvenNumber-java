f31467b1070675e39c5d72ccacabc07a799c16db
locutus1986 <locutus1986@localhost.localdomain>
2019-03-12 23:06:11 -0400

    added a counter for even numbers found

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	    int x = 4;
+	    int counter = 0;
 	    int finishNumber = 20;
 
 	    while(x <= finishNumber){
@@ -11,8 +12,13 @@ public class Main {
 	        if(!isEvenNumber(x)){
 	            continue;
             }
+            counter++;
             System.out.println("Even number " + x);
+            if(counter >= 5){
+                break;
+            }
         }
+        System.out.println(counter + " even numbers were found");
     }
 
     public static boolean isEvenNumber(int x){
